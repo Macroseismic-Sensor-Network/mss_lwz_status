428b387e77b6d6bc59cbfd59ffd4dcf7c0fe1d75
Stefan Mertl <info@mertl-research.at>
2020-02-10 16:42:58 +0100

    Changed the software name in the license.

--- src/components/LWZServerState.vue
+++ src/components/LWZServerState.vue
@@ -2,23 +2,23 @@
 /*****************************************************************************
  * LICENSE
  *
- * This file is part of mss_vis.
+ * This file is part of mss_lwz_status.
  * 
- * If you use mss_vis in any program or publication, please inform and
+ * If you use mss_lwz_status in any program or publication, please inform and
  * acknowledge its authors.
  * 
- * mss_vis is free software: you can redistribute it and/or modify
+ * mss_lwz_status is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * mss_vis is distributed in the hope that it will be useful,
+ * mss_lwz_status is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with mss_vis. If not, see <http://www.gnu.org/licenses/>.
+ * along with mss_lwz_status. If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright 2019 Stefan Mertl
  *****************************************************************************/
